6be5cba651c341782c2ed5f3cfb4c63dfe4c1ded
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-22 17:36:16 +0800

    converting to class comp over functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// @ts-nocheck
+import React, { useState, Component } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor() {
+    super()
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    this.state = {
+      name: 'Wilfred'
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h2>Test</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
