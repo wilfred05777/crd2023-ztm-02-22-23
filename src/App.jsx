b13b8aee1efa9e0721f463ea1178732b22600bd0
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-26 06:09:32 +0800

    39. Monsters Rolodex - Renders & Re-renders in React

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ class App extends Component {
     super()
 
     this.state = {
-      // 37. Monsters Rolodex - Lifecycle Method: componentDidMount start
       monsters: []
     }
+    // 39. Monsters Rolodex - Renders & Re-renders in React 
+    // flow of react class
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((response) => response.json())
       .then((users) =>
@@ -26,19 +29,16 @@ class App extends Component {
           }
         )
       )
-    // .then((users) => console.log(users))
   }
-  // 37. Monsters Rolodex - Lifecycle Method: componentDidMount end
 
   handleClick() {
     console.log('handleClick')
   }
 
   render() {
+    console.log('render')
     return (
       <div className='App'>
-        {/* 33. Monsters Rolodex - Mapping Arrays to Elements start */}
-
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
@@ -46,41 +46,6 @@ class App extends Component {
             </div>
           )
         })}
-
-        {/* <h2>{this.state.monster1.name}</h2>
-        <h2>{this.state.monster2.name}</h2>
-        <h2>{this.state.monster3.name}</h2> */}
-        {/* 33. Monsters Rolodex - Mapping Arrays to Elements end*/}
-
-        {/* <header className='header'>
-          <h2>
-            Hi, {this.state.name.firstName} {this.state.name.lastName}, I work
-            at {this.state.company}!
-          </h2>
-
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: 'Julian Manuel', lastName: 'Bancairen' }
-                  }
-                },
-                // callback in class function
-                () => {
-                  console.log(this.state)
-                }
-              )
-              // this.setState({
-              //   // state and shallow Merge
-              //   name: { firstName: 'Rowena', lastName: 'Alicando' }
-              // })
-              // console.log(this.state)
-            }}
-          >
-            change name
-          </button>
-        </header> */}
       </div>
     )
   }
