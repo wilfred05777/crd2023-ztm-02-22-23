791c082d2cfb084481eafaf6961855712c660bdb
Wilfred <wilfred05777@gmail.com>
2023-02-28 12:06:43 +0800

    48. Monsters Rolodex - Rendering and Re-rendering part 2

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ class App extends Component {
   }
 
   render() {
-    // console.log('render')
+    console.log('render from AppJS')
 
     const { monsters, searchField } = this.state
     const { onSearchChange } = this
