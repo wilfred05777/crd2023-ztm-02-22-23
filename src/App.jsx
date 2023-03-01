11271efbdffd3ca2fa072d5f9cd32bc3609e212d
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-03-01 18:09:33 +0800

    56. Monsters Rolodex - Functional Component Intro

--- src/App.jsx
+++ src/App.jsx
@@ -6,72 +6,78 @@ import CardList from './components/card-list/card-list.component'
 import './App.css'
 import SearchBox from './components/search-box/search-box.component'
 
-class App extends Component {
-  constructor() {
-    super()
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    }
+      {/* <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monster'
+      />
 
-    // console.log('constructor')
-  }
-
-  componentDidMount() {
-    // console.log('componentDidMount')
-
-    // fetch(`https://pokeapi.co/api/v2/pokemon/ditto/name`)
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users }
-          }
-          //// example of call back function
-          //   () => {
-          //     console.log(this.state)
-          //   }
-        )
-      )
-  }
-
-  handleClick() {
-    console.log('handleClick')
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase()
-    this.setState(() => {
-      return { searchField }
-    })
-  }
-
-  render() {
-    console.log('render from AppJS')
-
-    const { monsters, searchField } = this.state
-    const { onSearchChange } = this
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField)
-    })
-
-    return (
-      <div className='App'>
-        <h1 className='app-title'>Monsters Rolodex</h1>
-
-        <SearchBox
-          className='monsters-search-box'
-          onChangeHandler={onSearchChange}
-          placeholder='search monster'
-        />
-
-        <CardList monsters={filteredMonsters} />
-      </div>
-    )
-  }
+      <CardList monsters={filteredMonsters} /> */}
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     super()
+
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     }
+
+//     // console.log('constructor')
+//   }
+
+//   componentDidMount() {
+//     // console.log('componentDidMount')
+
+//     // fetch(`https://pokeapi.co/api/v2/pokemon/ditto/name`)
+//     fetch(`https://jsonplaceholder.typicode.com/users`)
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users }
+//           }
+//           //// example of call back function
+//           //   () => {
+//           //     console.log(this.state)
+//           //   }
+//         )
+//       )
+//   }
+
+//   handleClick() {
+//     console.log('handleClick')
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase()
+//     this.setState(() => {
+//       return { searchField }
+//     })
+//   }
+
+//   render() {
+//     console.log('render from AppJS')
+
+//     const { monsters, searchField } = this.state
+//     const { onSearchChange } = this
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField)
+//     })
+
+//     return (
+
+//     )
+//   }
+// }
 
 export default App
